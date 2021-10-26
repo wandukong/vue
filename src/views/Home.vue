2d0f4e7cd83322f9cf53992a4650a4f991e9136a
wandukong <seungwan37@naver.com>
2021-10-26 21:49:09 +0900

    Starting Vue.js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="card">
+    <div class="card-header">
+      Home
+    </div>
+    <div class="card-body">
+      <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
