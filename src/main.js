a4d7f7e7969f476649a00b65a5267c917da5604c
wandukong <seungwan37@naver.com>
2021-11-02 23:52:14 +0900

    -

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 
 // Axios 설정 파일을 가져오면서 실행하기
-// axiois.defaults.baseURL을 먼저 적용
+// axios.defaults.baseURL을 먼저 적용
 import axios from './apis/axiosConfig'
 
 Vue.config.productionTip = false
